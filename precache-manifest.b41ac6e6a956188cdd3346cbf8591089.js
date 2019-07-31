ef39263d7f81a5d7855d49b4f1541d4135d86aa3
szamon <szamon@wp.pl>
2019-07-31 19:27:01 +0200

    Updates

---
+++ precache-manifest.b41ac6e6a956188cdd3346cbf8591089.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f0f60e65f5c58e5aeedb2c3776108b",
+    "url": "/DrumMachine/index.html"
+  },
+  {
+    "revision": "c4c6eec5a36f83d93d6c",
+    "url": "/DrumMachine/static/css/main.77597807.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/DrumMachine/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "c4c6eec5a36f83d93d6c",
+    "url": "/DrumMachine/static/js/main.8fbcbaa1.chunk.js"
+  },
+  {
+    "revision": "f9f965e18e8d27020dbc",
+    "url": "/DrumMachine/static/js/runtime~main.77d30c86.js"
+  }
+]);
